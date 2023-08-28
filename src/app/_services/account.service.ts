d4804fe120352e5b392a545ab9b01398e8f724f6
Mumtaz Ali <alimumtaz205@gmail.com>
2023-08-28 07:44:28 +0500

    pages added

---
+++ src/app/_services/account.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private userSubject: BehaviorSubject<User | null>;
+  public user: Observable<User | null>;
+  
+
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) { 
+    this.userSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('user')!));
+    this.user = this.userSubject.asObservable();
+  }
+
+  login(msidn: string, password: string) {
+    debugger;
+    return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, { msidn, password })
+        .pipe(map(user => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('user', JSON.stringify(user));
+            this.userSubject.next(user);
+            return user;
+        }));
+}
+}
